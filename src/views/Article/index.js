a1532702260e16150747e1df23cd6c0bdef39e7f
zhq092012 <zhq_092012@163.com>
2020-03-07 11:38:13 +0800

    1111

--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,60 +1,65 @@
 import React, { Component } from "react";
-import { Card, Button, Table } from "antd";
+import { Card, Button, Table, Tag } from "antd";
 import { getArticles } from "../../services";
-
+import moment from "moment";
+const titleDisplayMap = {
+  id: "序号",
+  title: "标题",
+  author: "作者",
+  createAt: "创建时间",
+  amount: "阅读量"
+};
 export default class ArticleList extends Component {
   constructor() {
     super();
     this.state = {
-      dataSource: [
-        {
-          key: "1",
-          name: "胡彦斌",
-          age: 32,
-          address: "西湖区湖底公园1号"
-        },
-        {
-          key: "2",
-          name: "胡彦祖",
-          age: 42,
-          address: "西湖区湖底公园1号"
-        }
-      ],
-      columns: [
-        {
-          title: "姓名",
-          dataIndex: "name",
-          key: "name"
-        },
-        {
-          title: "年龄",
-          dataIndex: "age",
-          key: "age"
-        },
-        {
-          title: "住址",
-          dataIndex: "address",
-          key: "address"
-        },
-        {
-          title: "操作",
-          dataIndex: "actions",
-          key: "actions",
-          render: (text, record, index) => {
-            return <Button>编辑</Button>;
-          }
-        }
-      ],
-      total: 0
+      dataSource: [],
+      columns: []
     };
   }
-  componentDidMount() {
+  createDisplayColumns = columnkeys => {
+    return columnkeys.map(item => {
+      if (item === "amount")
+        return {
+          title: titleDisplayMap[item],
+          key: item,
+          render: (text, record) => {
+            const { amount } = record;
+            return <Tag color={amount > 200 ? "red" : "green"}>{amount}</Tag>;
+          }
+        };
+      if (item === "createAt") {
+        return {
+          title: titleDisplayMap[item],
+          key: item,
+          render: (text, record) => {
+            const { createAt } = record;
+            return moment(createAt).format("YYYY年MM月DD日 HH:mm:ss");
+          }
+        };
+      }
+      return {
+        title: titleDisplayMap[item],
+        dataIndex: item,
+        key: item
+      };
+    });
+  };
+  getArticleList = () => {
     getArticles().then(res => {
+      const columnkeys = Object.keys(res.list[0]);
+      const columns = this.createDisplayColumns(columnkeys);
       this.setState({
-        total: res.total
+        total: res.total,
+        dataSource: res.list,
+        columns
       });
     });
+  };
+  componentDidMount() {
+    this.getArticleList();
   }
+
   render() {
     return (
       <Card
@@ -63,6 +68,7 @@ export default class ArticleList extends Component {
         extra={<Button>导出到Excel</Button>}
       >
         <Table
+          rowKey={record => record.id}
           dataSource={this.state.dataSource}
           columns={this.state.columns}
           pagination={{ total: this.state.total, hideOnSinglePage: true }}
